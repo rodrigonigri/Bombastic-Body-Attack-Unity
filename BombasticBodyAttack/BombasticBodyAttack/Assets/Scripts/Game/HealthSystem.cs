b8a6b908384c71f1133b75f957bd2db87c3deff6
Rodrigo Nigri Griner <rodrigong1@al.insper.edu.br>
2023-04-14 16:21:36 -0300

    logica das vidas implementada

---
+++ BombasticBodyAttack/BombasticBodyAttack/Assets/Scripts/Game/HealthSystem.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthSystem : MonoBehaviour
+{
+    // the UI text for the health count
+    public TextMeshProUGUI txt_LifeCount;
+
+    // the default health count (used for initialization)
+    public int defaultHealthCount;
+
+    // the current health count
+    public int healthCount;
+
+    // init the health system (reset the health count)
+    public void Init(){
+        healthCount = defaultHealthCount;
+        txt_LifeCount.text = healthCount.ToString();
+        
+    }
+
+    // lose health count
+    public void LoseHealth(){
+        if (healthCount < 1){
+            return;
+        }
+        healthCount--;
+        txt_LifeCount.text = healthCount.ToString();
+
+        CheckHealthCount();
+    }
+
+
+    // Check health cout for losing
+    void CheckHealthCount(){
+        if (healthCount < 1){
+            // lose the game
+            Debug.Log("You Lose");
+            // Call reset values and stop the game from the manager
+
+        }
+        
+    }
+    
+}
