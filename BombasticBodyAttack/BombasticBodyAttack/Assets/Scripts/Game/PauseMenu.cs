5d60101c1cb094d5f3e659eb969ee5eec45cec70
carolina-hirschheimer <carolinah@al.insper.edu.br>
2023-04-28 05:09:19 -0300

    botao pause funcionando e telas intro ajustadas

---
+++ BombasticBodyAttack/BombasticBodyAttack/Assets/Scripts/Game/PauseMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        AudioListener.volume = 0;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        AudioListener.volume = 1;
+    }
+
+    public void Home(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
